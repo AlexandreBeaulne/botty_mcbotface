57f8121f1ca8cafd40434d0974487e9e1ce1c754
Alexandre Beaulne <alexandre.beaulne@gmail.com>
2016-06-04 14:19:14 +0000

    made grid search append to csv

--- src/gridsearch.py
+++ src/gridsearch.py
@@ -6,6 +6,7 @@ an exercise in overfitting
 import io
 import gzip
 import json
+import random
 import argparse
 import collections
 import pandas as pd
@@ -15,6 +16,9 @@ import backtest
 from strategy import Strategy
 from utils import Logger
 from report import compute_outcomes
+
+COLS = ['watch_threshold', 'watch_duration', 'slowdown_threshold',
+        'slowdown_duration', 'direction', 'timeout', 'return']
 
 if __name__ == '__main__':
 
@@ -37,17 +41,15 @@ if __name__ == '__main__':
     trds_df = pd.read_csv(args.trds, parse_dates=['ts']).set_index('ts')
 
     # enumerate parameter spaces
-    watch_thrshlds = [0.01, 0.02, 0.03, 0.04, 0.05, 0.075, 0.1]
-    watch_drtns = [5, 10, 15, 30, 45, 60, 90, 120, 180, 300]
-    slowdown_thrshlds = [0.001, 0.002, 0.003, 0.005, 0.0075, 0.01, 0.02, 0.04]
-    slowdown_drtns = [1, 2, 3, 4, 5, 7, 10, 15, 20, 30, 60]
-    exit_timeouts = [1, 2, 3, 5, 10, 20, 30, 40, 60, 90, 120, 180, 300]
-
-    outcomes_dfs = []
+    watch_thrshlds = [0.02, 0.035, 0.05, 0.075, 0.1]
+    watch_drtns = [5, 10, 30, 60, 90, 120, 300]
+    slowdown_thrshlds = [0.001, 0.002, 0.005, 0.01, 0.02, 0.04]
+    slowdown_drtns = [1, 2, 5, 10, 20, 30, 60]
+    exit_timeouts = [1, 2, 5, 10, 20, 30, 40, 60, 90, 120, 180]
 
     # loop over all possibilities
     combos = product(watch_thrshlds, watch_drtns, slowdown_thrshlds, slowdown_drtns)
-    for wt, wd, st, sd in combos:
+    for wt, wd, st, sd in random.shuffle(list(combos)):
         setup = {'type': 'backtest', 'watch_threshold': wt, 'watch_duration': wd,
                  'slowdown_threshold': st, 'slowdown_duration': sd}
         log.operation(setup)
@@ -59,8 +61,8 @@ if __name__ == '__main__':
                                    .assign(watch_duration=wd)
                                    .assign(slowdown_threshold=st)
                                    .assign(slowdown_duration=sd))
-        outcomes_dfs.append(outcomes_df)
-        log.operation('done')
 
-    pd.concat(outcomes_dfs).to_csv('gridsearch.csv', index=False)
+        (outcomes_df.loc[:, COLS]
+                    .to_csv('gridsearch.csv', index=False, mode='a', header=False))
+        log.operation('done')
 
